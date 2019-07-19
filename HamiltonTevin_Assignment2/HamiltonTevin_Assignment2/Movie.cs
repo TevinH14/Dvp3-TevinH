fa83d8f6db0e62da61bbfcbc0d3b071815451c9d
Tevin Hamilton <tahamilton@student.fullsail.edu>
2019-07-19 00:32:11 -0400

    new class has been added to project MovieArgs to save the data as a object
    new class has been added to project, member varibles have been created in order to store data and allow data to be recalled. A override method has been created to save the data name to list view.

---
+++ HamiltonTevin_Assignment2/HamiltonTevin_Assignment2/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamiltonTevin_Assignment2
+{
+   public  class MovieArgs: EventArgs
+   {
+        string _titel;
+        string _YearReleased;
+        string _publisher;
+        string _director;
+        string _genre;
+
+        public MovieArgs(string t,string y,string p,string d,string g)
+        {
+            _titel = t;
+            _YearReleased = y;
+            _publisher = p;
+            _director = d;
+            _genre = g;
+        }
+        public override string ToString()
+        {
+            return _titel;
+        }
+    }
+}
